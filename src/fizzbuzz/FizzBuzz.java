1e6d7a4fd7112ff0e84c5ba7c5cfb89b7a17bef8
Luciano Aguilar <lucianodavidaguilar21@gmail.com>
2021-08-24 19:12:04 -0300

    Se agrega el motodo

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -4,4 +4,12 @@ public class FizzBuzz {
 
 	String fizzBuzz ;
 
+	public void FizzB(int numero) {
+		for (int i = 1; i <=numero;i++) {
+			if(i % 3 == 0 && i % 5 == 0) {
+				fizzBuzz = "FizzBuzz";	
+				System.out.println(fizzBuzz);
+				}
+		}
+	}
 }
